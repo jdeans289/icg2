a51c31f0c02e7ac6b0ac7170700444152b456c8e
Deans <jdeans289@gmail.com>
2023-07-17 19:52:34 -0500

    Fix accidentally committed line

--- test/basic_checkpoint_restore/test_main.cpp
+++ test/basic_checkpoint_restore/test_main.cpp
@@ -11,10 +11,9 @@ TEST_F(ICGTest, CheckpointRestore) {
     memoryManager.declare_var("MyClass * ptr", &my_class_instance);
     
     // ACT
-    bool success = memoryManager.restore_checkpoint("checkpoint.txt");
+    memoryManager.restore_checkpoint("checkpoint.txt");
 
     // ASSERT
-    ASSERT_TRUE(success) << "Run this test from 'make check'/'make test'/'ctest' OR from the test/basic_checkpoint_restore directory";
     for (int i = 0; i < 5; i++) {
         ASSERT_EQ(my_class_instance->my_nested_class.my_arr[i], i+5);
     }
